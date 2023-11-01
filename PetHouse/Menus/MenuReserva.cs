ab61c9c6b7f15487923c60fdcfefb2b99e7dd061
Arthur Augusto Paula Hardman <112453063+arthurhardman@users.noreply.github.com>
2023-11-01 20:37:38 -0300

    Update MenuReserva.cs

--- PetHouse/Menus/MenuReserva.cs
+++ PetHouse/Menus/MenuReserva.cs
@@ -1,31 +1,54 @@
-﻿using PetHouse.Metodos;
-using System;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-namespace PetHouse.MenuReserva
+namespace PetHouse.Classes;
+
+internal class Reservas
 {
-    internal class MenuReserva
+    [JsonPropertyName("Numero")]
+    public int? numero { get; set; }
+
+    [JsonPropertyName("Quarto")]
+    public string? quarto { get; set; }
+
+    [JsonPropertyName("Data")]
+    public string? data { get; set; }
+
+    [JsonPropertyName("Valor")]
+    public int? valor { get; set; }
+
+    public void ExibirReservas()
     {
-        private GestaoReserva gestaoReserva = new GestaoReserva();
+        Console.WriteLine($"Numero da Reserva: {numero}");
+        Console.WriteLine($"Numero do Quarto: {quarto}");
+        Console.WriteLine($"Data da Reserva: {data}");
+        Console.WriteLine($"Valor da Reserva: {valor}");
+    }
 
-        public void AdicionarReserva(int numero, string quarto, string data)
+    public async Task AdicionarReserva()
+    {
+            // Coletar informações do usuário para a nova reserva
+            Console.WriteLine("Informe o número da reserva: ");
+            int numeroReserva = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Informe o número do quarto: ");
+            string numeroQuarto = Console.ReadLine()!;
+
+            Console.WriteLine("Informe a data da reserva: ");
+            string dataReserva = Console.ReadLine()!;
+
+            Console.WriteLine("Informe o valor da reserva: ");
+            int valorReserva = int.Parse(Console.ReadLine()!);
+
+        var novaReserva = new Reservas
         {
-            Reserva reserva = new Reserva(numero, quarto, data);
-            gestaoReserva.AdicionarReserva(reserva);
-            Console.WriteLine("Reserva adicionada com sucesso!");
-        }
-
-        public void ConsultarReserva(string numero)
-        {
-            Reserva reserva = gestaoReserva.ConsultarReserva(numero)!;
-            if (reserva != null)
-            {
-                Console.WriteLine($"Reserva encontrada: Número {reserva.Numero}, Quarto {reserva.Quarto}, Data {reserva.Data}");
-            }
-            else
-            {
-                Console.WriteLine("Reserva não encontrada.");
-            }
-        }
-
+            numero = numeroReserva,
+            quarto = numeroQuarto,
+            data = dataReserva,
+            valor = valorReserva
+        };
     }
 }
+
